8203f2cf41fa9ee8072a43252fde4f5d38df5822
nurasan <210103229@stu.sdu.edu.kz>
2023-11-29 16:23:36 +0600

    Adding Button Action for ExpandLess and ExpandMore

--- app/src/main/java/com/example/woof/MainActivity.kt
+++ app/src/main/java/com/example/woof/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -106,17 +107,19 @@ fun DogItem(
             DogIcon(dog.imageResourceId)
             DogInformation(dog.name, dog.age)
             Spacer(modifier = Modifier.weight(1f))
-            DogItemButton(expanded = expanded, onClick = { /*TODO*/ })
+            DogItemButton(expanded = expanded, onClick = { expanded = !expanded })
         }
-        DogHobby(
-            dog.hobbies,
-            modifier = Modifier.padding(
-                start = dimensionResource(R.dimen.padding_medium),
-                top = dimensionResource(R.dimen.padding_small),
-                end = dimensionResource(R.dimen.padding_medium),
-                bottom = dimensionResource(R.dimen.padding_medium)
+        if(expanded){
+            DogHobby(
+                dog.hobbies,
+                modifier = Modifier.padding(
+                    start = dimensionResource(R.dimen.padding_medium),
+                    top = dimensionResource(R.dimen.padding_small),
+                    end = dimensionResource(R.dimen.padding_medium),
+                    bottom = dimensionResource(R.dimen.padding_medium)
+                )
             )
-        )
+        }
     }
 }
 
@@ -128,10 +131,9 @@ private fun DogItemButton(
 ){
     IconButton(
         onClick = onClick,
-        modifier = modifier
     ) {
         Icon(
-            imageVector = Icons.Filled.ExpandMore ,
+            imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore ,
             contentDescription = stringResource(R.string.expand_button_content_description),
             tint = MaterialTheme.colorScheme.secondary
         )
